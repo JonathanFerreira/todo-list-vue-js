0d349d6dc9b972d2913e4d387c356b4b316ae619
JonathanFerreira <jonathan.vferreira1@gmail.com>
2017-08-16 19:24:34 -0300

    fix(CepChecker.vue): Correção ao exibir informaçõs do cep buscado

--- src/main.js
+++ src/main.js
@@ -8,8 +8,10 @@ import App from './App'
 // ---------------
 
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import routes from './routes'
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 const router = new VueRouter({
   mode: 'history',
